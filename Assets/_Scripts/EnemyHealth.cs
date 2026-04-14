b56fa47ddcf00fcebf6dedbc5f55e065caaa40b7
ahya-ah2204901 <ah2204901@qu.edu.qa>
2026-04-14 15:18:00 +0300

    fixed attack animations, working on attack dmg

---
+++ Assets/_Scripts/EnemyHealth.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] private int maxHealth = 100;
+    private int currentHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        //Debug.Log($"{gameObject.name} took {damage} damage. Health left: {currentHealth}");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        //Debug.Log($"{gameObject.name} died");
+        Destroy(gameObject);
+    }
+}
